91ff9b75e15f3551acc25c7c5ccd4ec4728185e6
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-09-03 17:21:22 -0400

    add - product reducer

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -5,6 +5,7 @@ import { SharedModule } from '../shared/shared.module';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductShellComponent } from './product-shell/product-shell.component';
+import { productReducer } from './state/product.reducer';
 
 const productRoutes: Routes = [{ path: '', component: ProductShellComponent }];
 
@@ -12,7 +13,7 @@ const productRoutes: Routes = [{ path: '', component: ProductShellComponent }];
   imports: [
     SharedModule,
     RouterModule.forChild(productRoutes),
-    StoreModule.forFeature('products', {}),
+    StoreModule.forFeature('products', productReducer),
   ],
   declarations: [
     ProductShellComponent,
