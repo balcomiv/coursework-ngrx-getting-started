c6eccae6428e73d18a9c9941221f89ab02d24fa7
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-09-04 15:18:21 -0400

    add - redux dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 // Imports for loading & configuring the in-memory web api
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { ProductData } from './products/product-data';
-
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-import { ShellComponent } from './home/shell.component';
 import { MenuComponent } from './home/menu.component';
-import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './home/page-not-found.component';
-
+import { ShellComponent } from './home/shell.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductData } from './products/product-data';
 /* Feature Modules */
 import { UserModule } from './user/user.module';
-import { StoreModule } from '@ngrx/store';
+
+
+
+
 
 @NgModule({
   imports: [
@@ -25,7 +27,11 @@ import { StoreModule } from '@ngrx/store';
     HttpClientInMemoryWebApiModule.forRoot(ProductData),
     UserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      name: 'APM Demo App Devtools',
+      maxAge: 25,
+      logOnly: environment.production })
   ],
   declarations: [
     AppComponent,
