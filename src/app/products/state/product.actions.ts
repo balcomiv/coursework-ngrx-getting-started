5069f06293e9b4801d9be1e7dffaf362d2ab9fb5
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-09-10 17:30:30 -0400

    add - actions for complex operations (load, load success, load failure)

--- src/app/products/state/product.actions.ts
+++ src/app/products/state/product.actions.ts
@@ -1,11 +1,14 @@
 import { createAction, props } from '@ngrx/store';
 import { Product } from '../product';
 
+//#region Toggle Product
 export const toggleProductCode = createAction('[Product] Toggle Product code');
+//#endregion
 
+//#region Current Product
 export const setCurrentProduct = createAction(
   '[Product] Set Current Product',
-  props<{ product: Product }>()
+  props<{ product: Product }>() //  metadata defining the structure of the returned data
 );
 
 export const clearCurrentProduct = createAction(
@@ -15,3 +18,19 @@ export const clearCurrentProduct = createAction(
 export const initializeCurrentProduct = createAction(
   '[Product] Initialize Current Product'
 );
+//#endregion
+
+//#region Load Product
+export const loadProduct = createAction('[Product] Load');
+
+export const loadProductSuccess = createAction(
+  '[Product] Load Success',
+  props<{ products: Product[] }>()
+);
+
+export const loadProductFailure = createAction(
+  '[Product] Load Failure',
+  props<{ error: string }>()
+);
+
+//#endregion
